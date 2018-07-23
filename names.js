408aafe22a9ad7c2e70675aacf81dd03a95136c4
dustinpfister <dustin.pfister@gmail.com>
2018-07-23 10:32:58 -0400

    names

--- names.js
+++ names.js
@@ -7,9 +7,6 @@ walker = walk.walk(process.argv[2] || process.cwd(), options);
 
 walker.on('names', function (root, names, next) {
 
-    //console.log(path.join(root, fileStats.name));
-
-    //console.log(fileStats.isDirectory());
     console.log('names at : ' + root);
     console.log(names);
     console.log('');
